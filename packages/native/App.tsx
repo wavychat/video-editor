8f5406d181078cd2f969b965aa823eca148930fa
Johann Buscail <johann@buscail.eu>
2021-05-21 17:16:47 +0200

    added webview

--- packages/native/App.tsx
+++ packages/native/App.tsx
@@ -1,21 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import WebView from "react-native-webview";
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style="auto" />
+		<View style={{ flex: 1, marginTop: 20 }}>
+			<WebView
+				source={{ uri: "https://google.com" }}
+				bounces={false}
+				scrollEnabled={false}
+				allowsInlineMediaPlayback
+			></WebView>
 		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
