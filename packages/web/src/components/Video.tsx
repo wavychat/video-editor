7652641704647e0cd6907388ba4ea1610b720a77
Johann Buscail <johann@buscail.eu>
2021-06-01 10:56:08 +0200

    improved video looping

--- packages/web/src/components/Video.tsx
+++ packages/web/src/components/Video.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useImperativeHandle, useRef } from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { variablesState } from "../helpers/atoms";
 
 interface Props {}
@@ -7,21 +7,40 @@ interface Props {}
 const BackgroundVideo = React.forwardRef<HTMLVideoElement, Props>(
 	(props, ref) => {
 		const videoRef = useRef<HTMLVideoElement>(null);
-		const variables = useRecoilValue(variablesState);
+		const [variables, setVariables] = useRecoilState(variablesState);
 
 		useImperativeHandle(ref, () => videoRef.current!);
 
 		useEffect(() => {
 			if (variables.playVideo) videoRef.current?.play();
-			else videoRef.current?.pause();
+			else {
+				console.log("stopped at frame", variables.pinnedFrame);
+				videoRef.current?.pause();
+			}
 		}, [variables.playVideo]);
+
+		useEffect(() => {
+			if (!videoRef.current) return;
+
+			const onEnd = () => {
+				videoRef.current!.currentTime = 0;
+				videoRef.current!.play();
+				setVariables((vars) => ({ ...vars, looped: vars.looped + 1 }));
+			};
+
+			videoRef.current.addEventListener("ended", onEnd);
+
+			return () => {
+				videoRef.current?.removeEventListener("ended", onEnd);
+			};
+		}, []);
 
 		return (
 			<div>
 				<video
 					src="./test.mp4"
-					loop
 					ref={videoRef}
+					muted={process.env.NODE_ENV === "development"}
 					style={{
 						position: "absolute",
 						zIndex: 1,
