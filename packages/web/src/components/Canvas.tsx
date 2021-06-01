b330565f6ca4c66fc3e3e2ce2e4c165f1ff566af
Johann Buscail <johann@buscail.eu>
2021-06-01 10:58:26 +0200

    added animation

--- packages/web/src/components/Canvas.tsx
+++ packages/web/src/components/Canvas.tsx
@@ -16,6 +16,7 @@ import {
 	lineOptionsState,
 	scriptState,
 } from "../helpers/atoms";
+import { IScripting } from "../helpers/types/scripting";
 
 interface Props {
 	onTextDoubleClick?: (obj: any) => any;
@@ -38,7 +39,7 @@ const EditCanvas = React.forwardRef<IEditCanvasRef, Props>(
 	({ onTextDoubleClick }, ref) => {
 		const lineOptions = useRecoilValue(lineOptionsState);
 		const [script, setScript] = useRecoilState(scriptState);
-		const variables = useRecoilValue(variablesState);
+		const [variables, setVariables] = useRecoilState(variablesState);
 
 		const currentFrameRef = useRef<number>();
 
@@ -57,6 +58,15 @@ const EditCanvas = React.forwardRef<IEditCanvasRef, Props>(
 			fabricCanvasRef.current.setWidth(w);
 			fabricCanvasRef.current.setHeight(h);
 			fabricCanvasRef.current.calcOffset();
+		};
+
+		const getObject = (id: string) => {
+			const objects = fabricCanvasRef.current?.getObjects() ?? [];
+
+			for (let i = 0; i < objects.length; i++) {
+				const object = objects[i];
+				if (object?.id === id) return object;
+			}
 		};
 
 		useImperativeHandle(ref, () => ({
@@ -78,21 +88,14 @@ const EditCanvas = React.forwardRef<IEditCanvasRef, Props>(
 				textbox.center();
 				return id;
 			},
-			getObject: (id: string) => {
-				const objects = fabricCanvasRef.current?.getObjects() ?? [];
-
-				for (let i = 0; i < objects.length; i++) {
-					const object = objects[i];
-					if (object?.id === id) return object;
-				}
-			},
 			getFabric: () => fabricCanvasRef.current,
 			getCanvas: () => canvasRef.current,
+			getObject,
 		}));
 
 		useEffect(() => {
-			currentFrameRef.current = variables.currentFrame;
-		}, [variables.currentFrame]);
+			currentFrameRef.current = variables.pinnedFrame;
+		}, [variables.pinnedFrame]);
 
 		useEffect(() => {
 			if (fabricCanvasRef.current)
@@ -152,7 +155,7 @@ const EditCanvas = React.forwardRef<IEditCanvasRef, Props>(
 			window.addEventListener("resize", resizeCanvas, false);
 
 			// TODO: handle events (onTextDoubleClick, onPath)
-			canvas.on("object:added", function (opt) {
+			canvas.on("object:added", (opt) => {
 				const id = uuidv4();
 
 				setScript((script) => {
@@ -163,6 +166,11 @@ const EditCanvas = React.forwardRef<IEditCanvasRef, Props>(
 					script_clone[frame] = {
 						...t,
 						show: [...(t?.show ?? []), id],
+					};
+
+					script_clone[frame + 20] = {
+						...t,
+						hide: [...(t?.hide ?? []), id],
 					};
 
 					return script_clone;
@@ -179,6 +187,77 @@ const EditCanvas = React.forwardRef<IEditCanvasRef, Props>(
 			return () => {
 				window.removeEventListener("resize", resizeCanvas, false);
 			};
+		}, []);
+
+		useEffect(() => {
+			let frame: number = 1;
+			let last_loop: number = 0;
+
+			/** Request Animation Frame on specific FPS */
+			const requestAnimFrame = () =>
+				setTimeout(() => {
+					requestAnimationFrame(animate);
+				}, 1000 / variables.FPS);
+
+			/** Increment frame and request animation frame */
+			const nextFrame = () => {
+				frame++;
+				return requestAnimFrame();
+			};
+
+			const animate = () => {
+				let notPlayingVideo: boolean = false;
+				let hasPinnedFrame: boolean = false;
+				let timesLooped: number = 0;
+				let _script: IScripting = {};
+
+				// TODO: Remove this and change ref when recoil state changes
+				setScript((script) => {
+					_script = script;
+					return script;
+				});
+
+				// Getting variables
+				setVariables((vars) => {
+					notPlayingVideo = !vars.playVideo;
+					hasPinnedFrame = !!vars.pinnedFrame;
+					timesLooped = vars.looped;
+					return vars;
+				});
+
+				// Reseting frame on reloop
+				if (last_loop < timesLooped) {
+					last_loop++;
+					frame = 1;
+				}
+
+				if (notPlayingVideo || hasPinnedFrame)
+					return requestAnimFrame();
+
+				const frame_script = _script[frame];
+
+				console.log(frame);
+
+				if (!frame_script) return nextFrame();
+
+				for (let to_hide of frame_script.hide || []) {
+					const obj = getObject(to_hide);
+					if (!obj) continue;
+					obj.opacity = 0;
+				}
+
+				for (const to_show of frame_script.show || []) {
+					const obj = getObject(to_show);
+					console.log(to_show, obj);
+					if (!obj) continue;
+					obj.opacity = 1;
+				}
+
+				fabricCanvasRef.current?.renderAll();
+				return nextFrame();
+			};
+
+			requestAnimationFrame(animate);
 		}, []);
 
 		return (
