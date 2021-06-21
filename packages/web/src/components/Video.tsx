1605350bcafcb1d70c65ab8f3ce467ad70d2f227
johannb75 <johann@buscail.eu>
2021-06-21 21:20:40 +0200

    Made canvas and video responsive

--- packages/web/src/components/Video.tsx
+++ packages/web/src/components/Video.tsx
@@ -1,101 +1,138 @@
-import React, { useEffect, useImperativeHandle, useRef } from "react";
-import { useRecoilState } from "recoil";
-import { variablesState } from "../helpers/atoms";
-import { IS_PROD } from "../helpers/constants";
+import React,
+{
+	useEffect,
+	useImperativeHandle,
+	useRef,
+}
+
+	from "react";
+
+import {
+	useRecoilState,
+}
+
+	from "recoil";
+
+import {
+	variablesState,
+}
+
+	from "../helpers/atoms";
+
+import {
+	IS_PROD,
+}
+
+	from "../helpers/constants";
 
 interface Props {}
 
-const BackgroundVideo = React.forwardRef<HTMLVideoElement, Props>(
-	(props, ref) => {
-		const videoRef = useRef<HTMLVideoElement>(null);
-		const [variables, setVariables] = useRecoilState(variablesState);
+const BackgroundVideo = React.forwardRef<HTMLVideoElement,
+Props>((props, ref) => {
+	const videoRef = useRef<HTMLVideoElement>(null);
+	const [variables, setVariables] = useRecoilState(variablesState);
 
-		useImperativeHandle(ref, () =>
-			(videoRef.current!));
+	useImperativeHandle(ref, () =>
+		(videoRef.current !));
 
-		useEffect(() => {
-			const video = videoRef.current;
-			if (!video)
-				return;
+	useEffect(() => {
+		const video = videoRef.current;
+		if (!video)
+			return;
 
-			if (variables.playVideo)
-				video.play();
-			else {
-				console.log("stopped at frame", variables.pinnedFrame);
+		if (variables.playVideo)
+			video.play();
+
+		else {
+			console.log("stopped at frame", variables.pinnedFrame);
+			video.pause();
+		}
+
+		/** Is the tab focused */
+		let focused: boolean = true;
+
+		/** Pause video when not in the tab */
+		const onVisibilityChange = () => {
+			focused = !focused;
+			if (!focused)
 				video.pause();
-			}
+			else if (variables.playVideo)
+				video.play();
+		};
+		document.addEventListener("visibilitychange", onVisibilityChange);
 
-			/** Is the tab focused */
-			let focused: boolean = true;
-			/** Pause video when not in the tab */
-			const onVisibilityChange = () => {
-				focused = !focused;
-				if (!focused)
-					video.pause();
-				else if (variables.playVideo)
-					video.play();
-			};
+		return () => {
+			document.removeEventListener("visibilitychange",
+				onVisibilityChange);
+		};
+	},
 
-			document.addEventListener("visibilitychange", onVisibilityChange);
+	[variables.playVideo]);
 
-			return () => {
-				document.removeEventListener(
-					"visibilitychange",
-					onVisibilityChange,
-				);
-			};
-		}, [variables.playVideo]);
+	useEffect(() => {
+		const video = videoRef.current;
+		if (!video)
+			return;
 
-		useEffect(() => {
-			const video = videoRef.current;
-			if (!video)
-				return;
+		/** On video loaded and ready to be played */
+		const onReady = () => {
+			setVariables((vars) =>
+				({
+					...vars,
+					playVideo: true,
+				}
 
-			/** On video loaded and ready to be played */
-			const onReady = () => {
-				setVariables((vars) =>
-					({
-						...vars,
-						playVideo: true,
-					}));
-			};
+				));
+		};
+		video.addEventListener("canplay", onReady);
 
-			video.addEventListener("canplay", onReady);
+		// for ios
+		video.load();
 
-			// for ios
-			video.load();
+		/** Replay video when ended (loop) */
+		const onEnd = () => {
+                    videoRef.current !.currentTime = 0;
+                    videoRef.current !.play();
+		};
+		video.addEventListener("ended", onEnd);
 
-			/** Replay video when ended (loop) */
-			const onEnd = () => {
-				videoRef.current!.currentTime = 0;
-				videoRef.current!.play();
-			};
+		return () => {
+			video.removeEventListener("canplay", onReady);
+			video.removeEventListener("ended", onEnd);
+		};
+	},
 
-			video.addEventListener("ended", onEnd);
+	[]);
 
-			return () => {
-				video.removeEventListener("canplay", onReady);
-				video.removeEventListener("ended", onEnd);
-			};
-		}, []);
+	return (
+		<div>
+			{" "}
+			<video
+				src="./test.mp4"
 
-		return (
-			<div>
-				<video
-					src="./test.mp4"
-					ref={videoRef}
-					muted={!IS_PROD}
-					style={{
+				ref={
+					videoRef
+				}
+
+				muted={
+					!IS_PROD
+				}
+
+				style={
+					{
 						position: "absolute",
 						zIndex: 1,
+						top: "50%",
 						left: "50%",
-						transform: "translate(-50%, 0)",
-					}}
-				/>
-			</div>
-		);
-	},
-);
+						transform: "translate(-50%, -50%)",
+					}
+				}
+			/>
+			{" "}
+
+		</div>
+	);
+});
 
 BackgroundVideo.displayName = "BackgroundVideo";
 
