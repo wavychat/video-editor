c6b7046d498b63f4a364d4ffd64af0cb1eaec883
johannb75 <johann@buscail.eu>
2021-06-23 10:27:59 +0200

    Added VideoEditor story

--- packages/web/src/stories/Button.stories.tsx
+++ packages/web/src/stories/Button.stories.tsx
@@ -1,38 +1,15 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react";
 
-import { Button, ButtonProps } from "./Button";
+import { VideoEditor } from "../VideoEditor";
 
 export default {
-	title: "Example/Button",
-	component: Button,
-	argTypes: {
-		backgroundColor: { control: "color" },
-	},
+	title: "Example/VideoEditor",
+	component: VideoEditor,
 } as Meta;
 
-const Template: Story<ButtonProps> = (args) =>
-	<Button {...args} />;
+const Template: Story = (args) =>
+	<VideoEditor />;
 
 export const Primary = Template.bind({});
-Primary.args = {
-	primary: true,
-	label: "Button",
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-	label: "Button",
-};
-
-export const Large = Template.bind({});
-Large.args = {
-	size: "large",
-	label: "Button",
-};
-
-export const Small = Template.bind({});
-Small.args = {
-	size: "small",
-	label: "Button",
-};
+Primary.args = {};
